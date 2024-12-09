0e9b68922a498aac67710ddc1f3f8809cd608e22
Ahmed Mohamady Shehata <major_2047@hotmail.com>
2024-12-09 15:36:29 +0200

    lesson 04

--- ThredsDotNet/Program.cs
+++ ThredsDotNet/Program.cs
@@ -6,32 +6,41 @@ namespace ThredsDotNet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Thread t = Thread.CurrentThread;
-            t.Name = "main thred";
-            
-            Console.WriteLine($"Thred name : {t.Name}");
-            Console.WriteLine($"Thred name : {Thread.CurrentThread.Name}");
+            Console.WriteLine("Main Theard Start");
+            Thread t1 = new Thread(mathod01)
+            {
+                Name = "tMathod01"
+            };
+            Thread t2 = new Thread(mathod02)
+            {
+                Name = "tMathod02"
+            };
+            Thread t3 = new Thread(mathod03)
+            {
+                Name = "tMathod03"
+            };
 
-            mathod01();
-            Console.WriteLine("End of M1");
-            mathod02();
-            Console.WriteLine("End of M2");
-            mathod03();
-            Console.WriteLine("End of M3");
+            t1.Start();
+            t2.Start();
+            t3.Start();
 
+            Console.WriteLine("Main Theard End");
         }
 
 
         static void mathod01()
         {
+            Console.WriteLine("Start of M1");
             for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine($"From m1 : {i}");
             }
+            Console.WriteLine("End of M1");
         }
+
         static void mathod02()
         {
+            Console.WriteLine("Start of M2");
             for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine($"From m2 : {i}");
@@ -42,14 +51,17 @@ namespace ThredsDotNet
                     Console.WriteLine("End from databse");
                 }
             }
+            Console.WriteLine("End of M2");
         }
 
         static void mathod03()
         {
+            Console.WriteLine("Start of M3");
             for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine($"From m3 : {i}");
             }
+            Console.WriteLine("End of M3");
         }
 
     }
