daf9a05228a0ea7d6421c543ddc0f757fa1ed7f5
mohamady-div <major_2047@hotmail.com>
2024-12-16 20:55:26 +0200

    lesson 8

--- ThredsDotNet/Program.cs
+++ ThredsDotNet/Program.cs
@@ -1,101 +1,58 @@
 ﻿
-
-using System.Diagnostics;
-
 namespace ThredsDotNet
 {
-    public delegate void CallBackDel(int sum); 
-
     internal class Program
     {
-        static object _lock = new object();
-        static int Count = 0;
+
+        static ManualResetEvent resetEvent = new ManualResetEvent(false);
+        static AutoResetEvent auto = new AutoResetEvent(true);
 
         static void Main(string[] args)
         {
             Console.WriteLine("Main Theard Start");
 
-            //Stopwatch sw = Stopwatch.StartNew();
-
-            //Thread t1 = new Thread(increment);
-            //Thread t2 = new Thread(increment);
-            //Thread t3 = new Thread(increment);
-
+            //Thread t1 = new Thread(method01);
             //t1.Start();
-            //t2.Start();
-            //t3.Start();
 
-            //t1.Join();
-            //t2.Join();
-            //t3.Join();
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    new Thread(method02).Start();
+            //}
 
-            //Console.WriteLine("The Total : {0}", Count);
-
-            //sw.Stop();
-            //Console.WriteLine("The time is {0}", sw.ElapsedTicks);
-
-            Thread t1 = new Thread(method01);
-            Thread t2 = new Thread(method02);
-
-            t1.Start();
-            t2.Start();
-
-            t1.Join();
-            t2.Join();
+            for (int i = 0; i < 5; i++)
+            {
+                new Thread(method00).Start();
+            }
 
             Console.WriteLine("Main Theard End");
         }
 
+        public static void method00()
+        {
+            Console.WriteLine("Method 0 wating");
+            auto.WaitOne();
+            Console.WriteLine("Method 0 working");
+
+            Thread.Sleep(5000);
+            Console.WriteLine("Method 0 Completed");
+            auto.Set();
+        }
+
         public static void method01()
         {
-            Monitor.Enter(_lock);
-            for (int i = 0; i < 5; i++)
-            {
-                Monitor.Pulse(_lock);
-                Console.WriteLine("Method 1 working : {0}", i);
-
-                Console.WriteLine("Method 1 Completed : {0}", i);
-                Monitor.Wait(_lock);
-            }
+            Console.WriteLine("Method 1 working");
+            resetEvent.Reset();
+            Thread.Sleep(5000);
+            Console.WriteLine("Method 1 Completed");
+            resetEvent.Set();
         }
 
         public static void method02()
         {
-            Monitor.Enter(_lock);
-            for (int i = 0; i < 5; i++)
-            {
-                Monitor.Pulse(_lock);
-                Console.WriteLine("Method 2 working : {0}", i);
-
-                Console.WriteLine("Method 2 Completed : {0}", i);
-                Monitor.Wait(_lock);
-            }
+            Console.WriteLine("Method 2 working");
+            resetEvent.WaitOne();
+            Console.WriteLine("Method 2 Completed");
         }
-
-
-        static void increment()
-        {
-            for (int i = 0; i < 500000; i++)
-            {
-                //Count++;
-                //Interlocked.Increment(ref Count);
-                //lock (obj)
-                //{ 
-                //   Count++;
-                //}
-                bool lockToken = false;
-                Monitor.Enter(_lock, ref lockToken);
-                try
-                {
-                    Count++;
-                }
-                finally { 
-                    if (lockToken)
-                        Monitor.Exit(_lock); 
-                }
-            }
-        }
-
     }
 }
 
