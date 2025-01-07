9163f730d78d94d16469b1058935b4bdedd54a0b
mohamady-div <major_2047@hotmail.com>
2025-01-07 14:23:08 +0200

    Lesson 18

--- ThredsDotNet/Program.cs
+++ ThredsDotNet/Program.cs
@@ -10,24 +10,28 @@ namespace ThredsDotNet
         {
             Console.WriteLine(" {0} main thread start  ", Thread.CurrentThread.ManagedThreadId);
 
-            Stopwatch sw = Stopwatch.StartNew();
-            Console.WriteLine("start noraml loop C#");
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine(" {0}  thread working on val {1}  ", Thread.CurrentThread.ManagedThreadId, i);
-                Thread.Sleep(100);
-            }
-            sw.Stop();
-            Console.WriteLine("time is {0} ",sw.ElapsedMilliseconds);
-
-            sw = Stopwatch.StartNew();
             Console.WriteLine("start parallel loop C#");
-            Parallel.For(0, 10, x => {
-                Console.WriteLine(" {0} parallel working on val {1}  ", Thread.CurrentThread.ManagedThreadId, x);
-                Thread.Sleep(100);
+            ParallelOptions parallelOptions = new ParallelOptions() { 
+               MaxDegreeOfParallelism = -1,
+            };
+
+            int count = 0;
+
+            Parallel.For(0, 10, parallelOptions,  (x, breakLoop) => {
+
+                count += 1;
+                if (count >= 5)
+                {
+                    Console.WriteLine("loop will break {0}", count);
+                    breakLoop.Break();
+                }
+                else
+                {
+                    Console.WriteLine(" {0} parallel working on val {1}  ", Thread.CurrentThread.ManagedThreadId, count);
+                    Thread.Sleep(100);
+                }
             });
-            sw.Stop();
-            Console.WriteLine("time is {0} ", sw.ElapsedMilliseconds);
+
 
             Console.WriteLine(" {0} main thread end  ", Thread.CurrentThread.ManagedThreadId);
             Console.ReadKey();
